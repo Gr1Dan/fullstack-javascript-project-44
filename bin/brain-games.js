39228ce283a7cf6a0ab0fdb0824f0a28d0072dfa
GriDan <ddd1444ddd@gmail.com>
2025-01-01 20:18:24 +0500

    add a function to get a name

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
+import readlineSync from "readline-sync";
 
 console.log('node bin/brain-games.js');
 console.log('Welcome to the Brain Games!');
-
+const name = readlineSync.question('What is your name?');
+console.log(`Hello, ${name}`);
