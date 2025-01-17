7ce2e7c40e7268f9afc128130009d3b0bf285092
GriDan <ddd1444ddd@gmail.com>
2025-01-17 19:48:25 +0500

    Fixed a fail during the test

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -3,7 +3,7 @@ import readlineSync from "readline-sync";
 
 console.log('node bin/brain-games.js');
 console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('What is your name?');
+const name = readlineSync.question('May I have your name?');
 console.log(`Hello, ${name}`);
 export{
     name,
