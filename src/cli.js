c07a5b90e45e6458914f0866c8f2bc1481c09656
GriDan <ddd1444ddd@gmail.com>
2025-06-20 19:25:36 +0500

    add cli.js

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+export default function welcome() {
+    console.log('brain-games');
+    console.log("Welcome to the Brain Games!");
+    const name = readlineSync.question('May I have your name?: ');
+    console.log(name);
+}
