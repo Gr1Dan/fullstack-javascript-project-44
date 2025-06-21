8c45c8526033eb51a25c2355b5ae1af3a2a8efe7
GriDan <ddd1444ddd@gmail.com>
2025-06-21 16:54:49 +0500

    fix

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,10 @@
 import readlineSync from 'readline-sync';
 
 export default function welcome() {
-    console.log('node bin/brain-games.js\n');
-    console.log('brain-games\n');
-    console.log("Welcome to the Brain Games!\n");
+    console.log('node bin/brain-games.js');
+    console.log('brain-games');
+    console.log("Welcome to the Brain Games!");
     const name = readlineSync.question('May I have your name?: ');
+    console.log(`Hello, ${name}`);
     console.log(name);
 }
