2d27a6d47ec2820b1a149c9abc6e66b61e2e9f30
GriDan <ddd1444ddd@gmail.com>
2025-06-22 22:47:53 +0500

    Fix

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import calculateGame from "../src/Games/calc-game.js";
 
 calculateGame();
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,2 +1,4 @@
+#!/usr/bin/env node
+
 import evenGame from "../src/Games/even-game.js";
 evenGame();
