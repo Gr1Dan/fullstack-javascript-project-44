0c79c490d8a0b2d2445d38eb5d75335910d6fa0c
GriDan <ddd1444ddd@gmail.com>
2025-06-24 15:29:36 +0500

    fix

--- src/Games/prime-game.js
+++ src/Games/prime-game.js
@@ -1,22 +1,20 @@
-import {randomNum} from "../randomNum.js";
-import gameCount from "../index.js";
+import { randomNum } from '../randomNum.js'
+import gameCount from '../index.js'
 
-const gameRule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const gameRule = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
-// проверка числа: простое или нет
 const isPrime = (num) => {
-    if (num === 1) {
-        return 'yes';
-    }
-    for (let del = num - 1; del > 1; del -= 1) {
-        if (num % del === 0) {
+  if (num === 1) {
+    return 'yes';
+  }
+  for (let del = num - 1; del > 1; del -= 1) {
+    if (num % del === 0) {
             return 'no';
         }
     }
     return 'yes';
 };
 
-// генерация случайного числа и его проверка
 const gameData = () => {
     const num = randomNum();
     const task = num;
@@ -24,7 +22,6 @@ const gameData = () => {
     return [task, expectedAnswer];
 };
 
-// игра: простое ли число?
 const primeGame = () => gameCount(gameRule, gameData);
 
 export default primeGame;
