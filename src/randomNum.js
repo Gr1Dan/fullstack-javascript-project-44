4ff014606f2c31ed0f1f4331b4abb23923b8d3e9
GriDan <ddd1444ddd@gmail.com>
2025-06-26 20:40:39 +0500

    fix

--- src/randomNum.js
+++ src/randomNum.js
@@ -1,16 +1,13 @@
-export const randomNum = () => Math.round(Math.random() * 101);
+export const randomNum = () => Math.round(Math.random() * 101)
 
-
-export const randomNumOf10 = () => Math.round(Math.random() * 11);
-
+export const randomNumOf10 = () => Math.round(Math.random() * 11)
 
 export const randomItemOfArr = (arr) => {
-    const randomIndex = Math.floor(Math.random() * arr.length);
-    return arr[randomIndex];
+  const randomIndex = Math.floor(Math.random() * arr.length)
+  return arr[randomIndex]
 };
 
-
 export const randomNumAndItemOfArr = (arr) => {
-    const randomIndex = Math.floor(Math.random() * arr.length);
-    return [randomIndex, arr[randomIndex]];
+  const randomIndex = Math.floor(Math.random() * arr.length)
+  return [randomIndex, arr[randomIndex]]
 };
