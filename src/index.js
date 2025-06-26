44e501f559210d230c153d528acb6b20d8d5f0bd
GriDan <ddd1444ddd@gmail.com>
2025-06-26 20:47:48 +0500

    fix

--- src/index.js
+++ src/index.js
@@ -1,26 +1,23 @@
-import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync'
 
-const roundsCount = 3;
+const roundsCount = 3
 
 const gameCount = (gameRule, gameData) => {
-    console.log('Welcome to the Brain Games!');
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
-    console.log(gameRule);
-    let round = 0;
-    while (round < roundsCount) {
-        const [task, expectedAnswer] = gameData();
-        console.log(`Question: ${task}`);
-        const userAnswer = readlineSync.question('Your answer: ');
-        if (expectedAnswer === userAnswer) {
-            console.log('Correct!');
-            round += 1;
-        } else {
-            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'.\nLet's try again, ${userName}!`);
-            return;
-        }
+  console.log('Welcome to the Brain Games!')
+  const userName = readlineSync.question('May I have your name? ')
+  console.log(`Hello, ${userName}!`)
+  console.log(gameRule)
+  let round = 0;
+  while (round < roundsCount) {
+    const [task, expectedAnswer] = gameData();
+    console.log(`Question: ${task}`)
+    const userAnswer = readlineSync.question('Your answer: ')
+    if (expectedAnswer === userAnswer) {
+      console.log('Correct!')
+      round += 1}
+    else {console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'.\nLet's try again, ${userName}!`);return}
     }
-    console.log(`Congratulations, ${userName}!`);
-};
+    console.log(`Congratulations, ${userName}!`)
+}
 
-export default gameCount;
+export default gameCount
--- src/randomNum.js
+++ src/randomNum.js
@@ -5,9 +5,9 @@ export const randomNumOf10 = () => Math.round(Math.random() * 11)
 export const randomItemOfArr = (arr) => {
   const randomIndex = Math.floor(Math.random() * arr.length)
   return arr[randomIndex]
-};
+}
 
 export const randomNumAndItemOfArr = (arr) => {
   const randomIndex = Math.floor(Math.random() * arr.length)
   return [randomIndex, arr[randomIndex]]
-};
+}
